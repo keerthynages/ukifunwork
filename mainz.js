1f077c93d7a48785f2168a4028e6ed4f51fa73f1
keerthynages <keerthynages@gmail.com>
2019-07-09 01:43:12 +0530

    funs

---
+++ mainz.js
@@ -0,0 +1,258 @@
+
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+$(document).ready(function() {
+
+
+
+  SC.stream('/tracks/9261920',function(sound){
+
+       $("#start").click(function(e) {
+              e.preventDefault();
+              sound.start();
+             });
+
+
+              $("#stop").click(function(e) {
+                 e.preventDefault();
+                 sound.stop();
+               });
+});
+
+
+//dheiva thirumagal
+SC.stream('/tracks/97112786',function(sound){
+
+     $("#start1").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop1").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+//thangameenkal
+
+
+SC.stream('/tracks/107812013',function(sound){
+
+     $("#start2").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop2").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+//vishvasam
+
+SC.stream('/tracks/599362299',function(sound){
+
+     $("#start3").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop3").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+
+//kgf
+
+SC.stream('/tracks/583641381',function(sound){
+
+     $("#start4").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop4").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+//shape of you
+
+SC.stream('/tracks/301161123',function(sound){
+
+     $("#start5").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop5").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+
+
+//Barbie
+
+SC.stream('/tracks/146782566',function(sound){
+
+     $("#start6").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop6").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+
+//rasputin
+
+SC.stream('/tracks/223164134',function(sound){
+
+     $("#start7").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop7").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+
+
+//thinking out loud
+
+SC.stream('/tracks/175651162',function(sound){
+
+     $("#start8").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop8").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+
+
+// chill nation
+
+SC.stream('/tracks/267876616',function(sound){
+
+     $("#start9").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop9").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+
+
+//guru
+
+SC.stream('/tracks/602054046',function(sound){
+
+     $("#start10").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop10").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+
+//aashuqui 2
+
+SC.stream('/tracks/584885949',function(sound){
+
+     $("#start11").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop11").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+//Jhoda Akbhar
+
+SC.stream('/tracks/200200078',function(sound){
+
+     $("#start12").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop12").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+
+//jeni mere ahani
+
+SC.stream('/tracks/323174483',function(sound){
+
+     $("#start13").click(function(e) {
+            e.preventDefault();
+            sound.start();
+           });
+
+
+            $("#stop13").click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+});
+
+
+
+
+});
